99cf850dcd92f201960f6fb9e7b101ae1767090a
unknown <istartgame31@gmail.com>
2025-10-03 16:13:25 +0700

    add panic safe to lib

--- src/get_episode_list.rs
+++ src/get_episode_list.rs
@@ -1,5 +1,7 @@
 use std::ffi::{CString, CStr};
 use std::os::raw::{c_char};
+use std::panic;
+use std::ptr;
 
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, from_str};
@@ -35,104 +37,112 @@ struct Arguments {
 pub extern "C" fn get_episode_list(
     arguments_ptr : *const c_char,
 ) -> *const c_char {
+    let result = panic::catch_unwind(|| {
 
-    let mut return_result = ReturnResult {
-        status: false,
-        message: String::from(""),
-        data: Vec::new(),
-    };
-
-    // Check argument before processing
-    let mut valid_arguments: bool = true;
-    if arguments_ptr.is_null() {
-        return_result.message = String::from("Expected 1 argument.");
-        valid_arguments = false;
-    }
-
-    let mut args: Arguments = Arguments { 
-        id: String::from("") 
-    };
-    if valid_arguments {
-        unsafe { 
-            match from_str::<Arguments>(&CStr::from_ptr(arguments_ptr as *mut c_char).to_string_lossy().into_owned()) {
-                Ok(result) => {
-                    args.id = result.id
-                },
-                Err(e) => {
-                    return_result.message = String::from(e.to_string());
-                    valid_arguments = false;
-                }
-            }
+        let mut return_result = ReturnResult {
+            status: false,
+            message: String::from(""),
+            data: Vec::new(),
         };
-    }
-    // ================================================
 
-    if valid_arguments {
+        // Check argument before processing
+        let mut valid_arguments: bool = true;
+        if arguments_ptr.is_null() {
+            return_result.message = String::from("Expected 1 argument.");
+            valid_arguments = false;
+        }
 
-        let id = args.id;
+        let mut args: Arguments = Arguments { 
+            id: String::from("") 
+        };
+        if valid_arguments {
+            unsafe { 
+                match from_str::<Arguments>(&CStr::from_ptr(arguments_ptr as *mut c_char).to_string_lossy().into_owned()) {
+                    Ok(result) => {
+                        args.id = result.id
+                    },
+                    Err(e) => {
+                        return_result.message = String::from(e.to_string());
+                        valid_arguments = false;
+                    }
+                }
+            };
+        }
+        // ================================================
 
-        let client = reqwest::blocking::Client::new();
-        let mut headers = HeaderMap::new();
-        headers.insert(REFERER, HeaderValue::from_str(SOURCE_REFERER).unwrap());
-        headers.insert(HOST, HeaderValue::from_str(SOURCE_HOST).unwrap());
+        if valid_arguments {
 
-        // https://hianime.to/ajax/v2/episode/list/100
-        let url = format!("https://{}/ajax/v2/episode/list/{}", 
-            SOURCE_HOST, encode(&id)
-        );
-        
-        let res = client.get(&url).headers(headers).send().unwrap();
-        
-        if res.status().is_success(){
-            let data = res.json::<Value>().unwrap_or(Value::Null);
+            let id = args.id;
 
-            let root = Vis::load(data.get("html").unwrap_or(&Value::Null).as_str().unwrap_or("")).unwrap();
+            let client = reqwest::blocking::Client::new();
+            let mut headers = HeaderMap::new();
+            headers.insert(REFERER, HeaderValue::from_str(SOURCE_REFERER).unwrap());
+            headers.insert(HOST, HeaderValue::from_str(SOURCE_HOST).unwrap());
 
-            let mut episode_page_list: Vec<Vec<ReturnData>> = Vec::new();
+            // https://hianime.to/ajax/v2/episode/list/100
+            let url = format!("https://{}/ajax/v2/episode/list/{}", 
+                SOURCE_HOST, encode(&id)
+            );
+            
+            let res = client.get(&url).headers(headers).send().unwrap();
+            
+            if res.status().is_success(){
+                let data = res.json::<Value>().unwrap_or(Value::Null);
 
-            let mut ep_index:usize = 0;
+                let root = Vis::load(data.get("html").unwrap_or(&Value::Null).as_str().unwrap_or("")).unwrap();
 
-            for ep_page_ele in root.find(".ss-list") {
-                let mut episode_per_page: Vec<ReturnData> = Vec::new();
-                let ep_page_node = Vis::dom(&ep_page_ele);
+                let mut episode_page_list: Vec<Vec<ReturnData>> = Vec::new();
 
-                for ep_ele in ep_page_node.find(".ssl-item ") {
-                    let ep_node = Vis::dom(&ep_ele);
+                let mut ep_index:usize = 0;
 
-                    let mut id = String::from("");
-                    match ep_node.attr("data-id") {
-                        Some(result) => {
-                            id = result.to_string();
-                        },
-                        None => {}
+                for ep_page_ele in root.find(".ss-list") {
+                    let mut episode_per_page: Vec<ReturnData> = Vec::new();
+                    let ep_page_node = Vis::dom(&ep_page_ele);
+
+                    for ep_ele in ep_page_node.find(".ssl-item ") {
+                        let ep_node = Vis::dom(&ep_ele);
+
+                        let mut id = String::from("");
+                        match ep_node.attr("data-id") {
+                            Some(result) => {
+                                id = result.to_string();
+                            },
+                            None => {}
+                        }
+
+                        let mut title = String::from("");
+                        match ep_node.attr("title") {
+                            Some(result) => {
+                                title = result.to_string();
+                            },
+                            None => {}
+                        }
+                    
+                        episode_per_page.push(ReturnData {
+                            index: ep_index,
+                            id,
+                            title,
+                        });
+
+                        ep_index += 1;
                     }
 
-                    let mut title = String::from("");
-                    match ep_node.attr("title") {
-                        Some(result) => {
-                            title = result.to_string();
-                        },
-                        None => {}
-                    }
-                
-                    episode_per_page.push(ReturnData {
-                        index: ep_index,
-                        id,
-                        title,
-                    });
-
-                    ep_index += 1;
+                    episode_page_list.push(episode_per_page);
                 }
 
-                episode_page_list.push(episode_per_page);
+                return_result.data = vec![episode_page_list];
+                return_result.status = true;
             }
-
-            return_result.data = vec![episode_page_list];
-            return_result.status = true;
         }
-    }
+        
+        return serde_json::to_string(&return_result).unwrap();
+    });
     
-    let json_string = serde_json::to_string(&return_result).unwrap();
-    let result = CString::new(json_string).unwrap();
-    return result.into_raw();
+    match result {
+        Ok(data) => {
+            let result = CString::new(data).unwrap();
+            return result.into_raw();
+        },
+        _ => ptr::null(),
+    }
 }
--- src/get_server.rs
+++ src/get_server.rs
@@ -1,5 +1,8 @@
 use std::ffi::{CString, CStr};
 use std::os::raw::c_char;
+use std::panic;
+use std::ptr;
+
 
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, from_str};
@@ -10,6 +13,207 @@ use visdom::types::{BoxDynError, BoxDynElement};
 use url::Url;
 
 use crate::{SERVER_HOST, SERVER_ORIGIN, SERVER_REFERER, SOURCE_HOST, SOURCE_REFERER};
+
+
+#[derive(Serialize, Deserialize)]
+struct ReturnConfig {
+    host: String,
+    referer: String,
+    origin: String,
+    playlist_base_url: String,
+    segment_base_url: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct ReturnResult {
+    status: bool,
+    message: String,
+    data: Value,
+    config: ReturnConfig,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Arguments {
+    id: String
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn get_server(
+    arguments_ptr: *const c_char,
+) -> *const c_char {
+    let result = panic::catch_unwind(|| {
+        let mut return_result = ReturnResult {
+            status: false,
+            message: String::from(""),
+            data: Value::Null,
+            config: ReturnConfig {
+                host: String::from(""),
+                referer: SERVER_REFERER.to_string(),
+                origin: SERVER_ORIGIN.to_string(),
+                playlist_base_url: String::from(""),
+                segment_base_url: String::from("")
+            }
+        };
+
+        // Check argument before processing
+        let mut valid_arguments: bool = true;
+        if arguments_ptr.is_null() {
+            return_result.message = String::from("Expected 1 argument.");
+            valid_arguments = false;
+        }
+        
+        let mut args: Arguments = Arguments { id: String::from("") };
+        if valid_arguments {
+            unsafe { 
+                match from_str::<Arguments>(&CStr::from_ptr(arguments_ptr as *mut c_char).to_string_lossy().into_owned()) {
+                    Ok(result) => {
+                        args.id = result.id
+                    },
+                    Err(e) => {
+                        return_result.message = String::from(e.to_string());
+                        valid_arguments = false;
+                    }
+                }
+            };
+        }
+        // ================================================
+
+        if valid_arguments {
+            
+            let server_id = args.id;
+
+            let client = reqwest::blocking::Client::new();
+
+
+            // Get forward episode id
+            let mut forward_server_id: Option<String> = None;
+            let mut headers = HeaderMap::new();
+
+            headers.insert(REFERER, HeaderValue::from_str(&SOURCE_REFERER).unwrap());
+            headers.insert(HOST, HeaderValue::from_str(&SOURCE_HOST).unwrap());
+            
+            let url = format!("https://{}/ajax/v2/episode/sources?id={}", &SOURCE_HOST, &server_id);
+            let res = client.get(url)
+                .headers(headers.clone())
+                .send().unwrap();
+
+            if res.status().is_success() {
+                let data = res.json::<Value>();
+                match data {
+                    Ok(result) => {
+                        forward_server_id = Some(result.get("link").unwrap().to_string()
+                            .split("/").last().unwrap().split("?").nth(0).unwrap().to_string());
+                    }
+                    _ => {
+                        return_result.status = false;
+                        return_result.message = String::from("Failed to get forward episode id.");
+                        println!("[Failed] Failed to get forward episode id.");
+                    }
+                }
+            }
+
+            // ================================================
+
+
+
+            // Get html dom then extract token.
+            // println!("Forward episode id: {:?}", forward_server_id.as_ref().clone());
+            let mut token: Option<String> = None;
+            if forward_server_id.is_some() {
+                let mut headers = HeaderMap::new();
+
+                headers.insert(REFERER, HeaderValue::from_str(&SERVER_REFERER).unwrap());
+                headers.insert(HOST, HeaderValue::from_str(&SERVER_HOST).unwrap());
+                
+                let url = format!("https://{}/embed-2/v3/e-1/{}?k=1", &SERVER_HOST, forward_server_id.as_ref().unwrap());
+
+                let res = client.get(url)
+                    .headers(headers.clone())
+                    .send().unwrap();
+
+                if res.status().is_success() {
+                    let body = res.text();
+                    // println!("Body: {}", body.as_ref().unwrap());
+                    match extract_key_token(body.as_ref().unwrap()) {
+                        Ok(Some(result)) => {
+                            token = Some(result);
+                        }
+                        _ => {
+                            return_result.status = false;
+                            
+                            println!("[Not Found] Failed to extract key token.");
+                        }
+                    }
+                }
+            }
+            // ================================================
+
+            // Get source info from generated token.
+            // println!("Token: {:?}", token.clone());
+            if token.is_some() {
+                let mut headers = HeaderMap::new();
+                headers.insert(REFERER, HeaderValue::from_str(&SERVER_REFERER).unwrap());
+                headers.insert(HOST, HeaderValue::from_str(&SERVER_HOST).unwrap());
+                let url = format!("https://{}/embed-2/v3/e-1/getSources?id={}&_k={}", &SERVER_HOST, forward_server_id.as_ref().unwrap(), &token.unwrap());
+                let res = client.get(url)
+                .headers(headers.clone())
+                .send().unwrap();
+
+                if res.status().is_success() {
+                    let data = res.json::<Value>();
+                    match data {
+                        Ok(result) => {
+                            let server = result.get("server").unwrap().as_u64().unwrap() as usize;
+                            let file_url = result.get("sources").unwrap().get(0).unwrap().get("file").unwrap().as_str().unwrap();
+                            let base_url  = file_url.split('/').collect::<Vec<_>>()[..file_url.matches('/').count()].join("/");
+                            if server == 1 {
+                                return_result.config.segment_base_url = base_url.clone();
+                            }
+
+                            match Url::parse(file_url) {
+                                Ok(parsed_url) => {
+                                    if let Some(host) = parsed_url.host_str() {
+                                        return_result.status = true;
+                                        if server != 6 {
+                                            return_result.config.playlist_base_url = base_url.clone();
+                                        }
+                                        
+                                        return_result.config.host = host.to_string();
+                                        return_result.data = result;
+                                        return_result.message = String::from("success");
+                                    } else {
+                                        return_result.message = String::from("Failed to get host.");
+                                    }
+                                }
+                                Err(e) => {
+                                    return_result.message = String::from(e.to_string());
+                                },
+                            }
+                            
+                        }
+                        _ => {
+                            return_result.status = false;
+                            println!("[Failed] Failed to get sources.");
+                        }
+                    }
+                }
+                
+            }
+
+            // ========================================
+        }
+        
+        return serde_json::to_string(&return_result).unwrap();
+    });
+
+    match result {
+        Ok(data) => {
+            let result = CString::new(data).unwrap();
+            return result.into_raw();
+        },
+        _ => ptr::null(),
+    }
+}
 
 
 fn extract_key_token(html: &str) -> Result<Option<String>, BoxDynError> {
@@ -79,199 +283,4 @@ fn extract_key_token(html: &str) -> Result<Option<String>, BoxDynError> {
     }
 
     Ok(None)
-}
-
-#[derive(Serialize, Deserialize)]
-struct ReturnConfig {
-    host: String,
-    referer: String,
-    origin: String,
-    playlist_base_url: String,
-    segment_base_url: String
-}
-
-#[derive(Serialize, Deserialize)]
-struct ReturnResult {
-    status: bool,
-    message: String,
-    data: Value,
-    config: ReturnConfig,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Arguments {
-    id: String
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn get_server(
-    arguments_ptr: *const c_char,
-) -> *const c_char {
-
-    let mut return_result = ReturnResult {
-        status: false,
-        message: String::from(""),
-        data: Value::Null,
-        config: ReturnConfig {
-            host: String::from(""),
-            referer: SERVER_REFERER.to_string(),
-            origin: SERVER_ORIGIN.to_string(),
-            playlist_base_url: String::from(""),
-            segment_base_url: String::from("")
-        }
-    };
-
-    
-
-    // Check argument before processing
-    let mut valid_arguments: bool = true;
-    if arguments_ptr.is_null() {
-        return_result.message = String::from("Expected 1 argument.");
-        valid_arguments = false;
-    }
-    
-    let mut args: Arguments = Arguments { id: String::from("") };
-    if valid_arguments {
-        unsafe { 
-            match from_str::<Arguments>(&CStr::from_ptr(arguments_ptr as *mut c_char).to_string_lossy().into_owned()) {
-                Ok(result) => {
-                    args.id = result.id
-                },
-                Err(e) => {
-                    return_result.message = String::from(e.to_string());
-                    valid_arguments = false;
-                }
-            }
-        };
-    }
-    // ================================================
-
-    if valid_arguments {
-        
-        let server_id = args.id;
-
-        let client = reqwest::blocking::Client::new();
-
-
-        // Get forward episode id
-        let mut forward_server_id: Option<String> = None;
-        let mut headers = HeaderMap::new();
-
-        headers.insert(REFERER, HeaderValue::from_str(&SOURCE_REFERER).unwrap());
-        headers.insert(HOST, HeaderValue::from_str(&SOURCE_HOST).unwrap());
-        
-        let url = format!("https://{}/ajax/v2/episode/sources?id={}", &SOURCE_HOST, &server_id);
-        let res = client.get(url)
-            .headers(headers.clone())
-            .send().unwrap();
-
-        if res.status().is_success() {
-            let data = res.json::<Value>();
-            match data {
-                Ok(result) => {
-                    forward_server_id = Some(result.get("link").unwrap().to_string()
-                        .split("/").last().unwrap().split("?").nth(0).unwrap().to_string());
-                }
-                _ => {
-                    return_result.status = false;
-                    return_result.message = String::from("Failed to get forward episode id.");
-                    println!("[Failed] Failed to get forward episode id.");
-                }
-            }
-        }
-
-        // ================================================
-
-
-
-        // Get html dom then extract token.
-        // println!("Forward episode id: {:?}", forward_server_id.as_ref().clone());
-        let mut token: Option<String> = None;
-        if forward_server_id.is_some() {
-            let mut headers = HeaderMap::new();
-
-            headers.insert(REFERER, HeaderValue::from_str(&SERVER_REFERER).unwrap());
-            headers.insert(HOST, HeaderValue::from_str(&SERVER_HOST).unwrap());
-            
-            let url = format!("https://{}/embed-2/v3/e-1/{}?k=1", &SERVER_HOST, forward_server_id.as_ref().unwrap());
-
-            let res = client.get(url)
-                .headers(headers.clone())
-                .send().unwrap();
-
-            if res.status().is_success() {
-                let body = res.text();
-                // println!("Body: {}", body.as_ref().unwrap());
-                match extract_key_token(body.as_ref().unwrap()) {
-                    Ok(Some(result)) => {
-                        token = Some(result);
-                    }
-                    _ => {
-                        return_result.status = false;
-                        
-                        println!("[Not Found] Failed to extract key token.");
-                    }
-                }
-            }
-        }
-        // ================================================
-
-        // Get source info from generated token.
-        // println!("Token: {:?}", token.clone());
-        if token.is_some() {
-            let mut headers = HeaderMap::new();
-            headers.insert(REFERER, HeaderValue::from_str(&SERVER_REFERER).unwrap());
-            headers.insert(HOST, HeaderValue::from_str(&SERVER_HOST).unwrap());
-            let url = format!("https://{}/embed-2/v3/e-1/getSources?id={}&_k={}", &SERVER_HOST, forward_server_id.as_ref().unwrap(), &token.unwrap());
-            let res = client.get(url)
-            .headers(headers.clone())
-            .send().unwrap();
-
-            if res.status().is_success() {
-                let data = res.json::<Value>();
-                match data {
-                    Ok(result) => {
-                        let server = result.get("server").unwrap().as_u64().unwrap() as usize;
-                        let file_url = result.get("sources").unwrap().get(0).unwrap().get("file").unwrap().as_str().unwrap();
-                        let base_url  = file_url.split('/').collect::<Vec<_>>()[..file_url.matches('/').count()].join("/");
-                        if server == 1 {
-                            return_result.config.segment_base_url = base_url.clone();
-                        }
-
-                        match Url::parse(file_url) {
-                            Ok(parsed_url) => {
-                                if let Some(host) = parsed_url.host_str() {
-                                    return_result.status = true;
-                                    if server != 6 {
-                                        return_result.config.playlist_base_url = base_url.clone();
-                                    }
-                                    
-                                    return_result.config.host = host.to_string();
-                                    return_result.data = result;
-                                    return_result.message = String::from("success");
-                                } else {
-                                    return_result.message = String::from("Failed to get host.");
-                                }
-                            }
-                            Err(e) => {
-                                return_result.message = String::from(e.to_string());
-                            },
-                        }
-                        
-                    }
-                    _ => {
-                        return_result.status = false;
-                        println!("[Failed] Failed to get sources.");
-                    }
-                }
-            }
-            
-        }
-
-        // ========================================
-    }
-    
-    let json_string = serde_json::to_string(&return_result).unwrap();
-    let result = CString::new(json_string).unwrap();
-    return result.into_raw();
 }
